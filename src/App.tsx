a99761c4ba6d59b6cd387e1c217694b81f55036a
Adam Lyons <AdamLyons.BYU@gmail.com>
2024-03-08 15:29:21 -0700

    Installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import collegeBasketBallTeams from "./CollegeBasketballTeams.json";
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>NCAA Basketball March Madness</h1>;
+}
+
+class Band extends React.Component<Team> {
+  render() {
+    const oneBand = this.props;
+
+    return (
+      <div>
+        <h2>School Name: {oneBand.school}</h2>
+        <h2>Mascot: {oneBand.name}</h2>
+        <h2>City: {oneBand.city}</h2>
+        <h2>State: {oneBand.state}</h2>
+      </div>
+    );
+  }
+}
+
+function Bandlist() {
+  return (
+    <div>
+      {collegeBasketBallTeams.teams.map((team: Team) => (
+        <Band key={team.school} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
